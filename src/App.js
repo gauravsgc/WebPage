6d7d07e237fe1d3ca1afead82700cdff726dc90b
Buvaneshvaran <52979995+MrX1068@users.noreply.github.com>
2021-02-25 15:25:29 +0530

    first commit started project.....

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Project-1</h1>
+      <p> Your code starts here ..... </p>
+
+    </div>
+  );
+}
+
+export default App;
